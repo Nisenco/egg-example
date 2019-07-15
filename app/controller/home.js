95017ab7d3551c317d797f9cf0bd3448a71fd819
nico <wangmeng1@social-touch.com>
2019-07-15 11:13:27 +0800

    添加 schedule 定时器任务

--- app/controller/home.js
+++ app/controller/home.js
@@ -1,28 +1,37 @@
 'use strict';
 const Controller = require('egg').Controller;
+
 class HomeController extends Controller {
   async index() {
     const { ctx } = this;
     ctx.body = 'home--index';
-  };
-  async list(){
-    const {ctx,service } = this;
+  }
+
+  async list() {
+    const { ctx, service } = this;
     ctx.body = 'home---list';
     const result = await service.user.creatData();
     ctx.body = result;
   }
-  async getData(){
-    const {ctx,service} = this;
+
+  async getData() {
+    const { ctx, service, app } = this;
     const pageIndex = ctx.query.pageIndex || '';
     const pageSize = ctx.query.pageSize || 20;
-    const dataList = await service.user.getData(pageIndex,pageSize);
-    const insertDataBase = await service.user.saveDB(dataList);
-    console.log(insertDataBase,'insertDataBase+++');
-    ctx.body = {
-      State: 0,
+    const dataList = await service.user.getData(pageIndex, pageSize);
+    await service.user.saveDB(dataList);
+    // const insertDataBase = await service.user.saveDB(dataList);
+    const errStatus = dataList.data;
+    let _length = dataList.data.length;
+    const temp = {
+      state: errStatus ? '0' : '10',
       Value: dataList,
-      Msg: '查询成功！'
+      Msg: errStatus ? '查询成功' : '查询失败',
     };
+    if (temp.state === '0') {
+      app.cache.lastCursor = dataList.data[_length - 1].order;
+    }
+    ctx.body = temp;
   }
 }
 
